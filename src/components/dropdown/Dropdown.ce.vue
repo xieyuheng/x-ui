ad15f4af4dd98e83653e43786733080cdbb1bdc7
Xie Yuheng <xyheme@gmail.com>
2023-06-02 23:27:49 +0800

    up

--- src/components/dropdown/Dropdown.ce.vue
+++ src/components/dropdown/Dropdown.ce.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    Dropdown TODO
+    <div>Dropdown TODO</div>
+    <div><slot name="head">head</slot></div>
+    <div><slot>body</slot></div>
+    <div><slot name="foot">foot</slot></div>
   </div>
 </template>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -7,7 +7,12 @@ import PageLayout from '../../layouts/page-layout/PageLayout.vue'
     <div class="flex flex-col overflow-y-auto p-3">
       <div>X UI</div>
 
-      <x-dropdown></x-dropdown>
+      <x-dropdown>
+        <div slot="head">head 1</div>
+        <div>body 1</div>
+        <div slot="foot">foot 1</div>
+        <div>body 2</div>
+      </x-dropdown>
     </div>
   </PageLayout>
 </template>
