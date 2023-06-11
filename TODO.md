97a9706413db5567b6c02edee9c2b6a7d7845641
Xie Yuheng <xyheme@gmail.com>
2023-06-11 15:10:50 +0800

    publish all components

--- TODO.md
+++
@@ -1,1 +0,0 @@
-publish all components
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import Watch from "./components/utils/Watch.vue"
+export { default as   Init}            from "./components/utils/Init.vue"
+export {default as  List}            from "./components/utils/List.vue"
+export {default as Popup}           from "./components/utils/Popup.vue"
+export {default as PopupSyncQuery}  from "./components/utils/PopupSyncQuery.vue"
+export {default as Render}          from "./components/utils/Render.vue"
+export {default as Scope}           from "./components/utils/Scope.vue"
+export {default as Watch}           from "./components/utils/Watch.vue"
 
-export { Watch }
